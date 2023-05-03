57b6ed78a30707c1ab5d4e0cdfcb21dc2e94023e
srgarcia94 <pgarciae01@gmail.com>
2023-05-03 17:15:16 -0400

    27

---
+++ wwr/First task.py
@@ -0,0 +1,13 @@
+color = input("Please type your favorite color: ")
+print("Your favorite color is")
+print(color)
+
+age = input ("How old are you? ")
+print("You are "+age+" old.")
+
+food = input("What is your favorite food? ")
+print("Your favorite food is "+food+". Delicious!")
+
+job = input("What is your job? ")
+print("Do you like your job? ")
+print("For me, your work as a "+job+" is interesting.")
